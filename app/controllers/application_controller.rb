86e9a650118799d286d999c022b83c2af3d8deb5
Carlos <carlosemmanuel.dev@gmail.com>
2022-04-04 10:42:02 -0300

    Alterando o controller da aplicação colocando novas configurações e métodos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: :home
+  before_action :access, only: :home
+
+  def after_sign_in_path_for(resource)
+    index_path
+  end
+
+  def access
+    redirect_to index_path if user_signed_in?
+  end
+
 end
